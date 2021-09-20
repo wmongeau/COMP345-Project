bbcbaa7e31393f8b253f138f1776b7ee650e1695
William Mongeau <williammongeau@gmail.com>
2021-09-20 12:20:22 -0400

    Reading a file works

--- Warzone/MapDriver.cpp
+++ Warzone/MapDriver.cpp
@@ -1,9 +1,25 @@
-/* ------------------------- */
-/* The #include Of this file */
-/* ------------------------- */
-
 #include <iostream>
 #include <string>
+#include <fstream> 
 
 using namespace std;
 #include "Map.h"
+
+int main(){
+	ifstream file("../bigeurope.map");
+	string line;
+	string currentMode;
+	while(getline(file, line)){
+		if(line[0] == ';'){
+			continue;
+		}
+		else if(line == "[continents]") {
+			currentMode = "continents";
+		}
+		cout << line << endl;
+		
+	}
+	file.close();
+	return 1;
+}
+
--- Warzone/Warzone.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "Warzone.h"
-
-using namespace std;
-
-int main()
-{
-	cout << "asdfsadFASDFASDFASDF" << endl;
-	return 0;
-}
