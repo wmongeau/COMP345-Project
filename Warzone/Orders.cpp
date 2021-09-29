dada7e92e23e103a8f8301ea1b11a667cb69a323
William Mongeau <williammongeau@gmail.com>
2021-09-29 14:25:50 -0400

    Functioning on Linux

--- Warzone/Orders.cpp
+++ Warzone/Orders.cpp
@@ -7,3 +7,300 @@
 
 using namespace std;
 #include "Orders.h"
+
+// ---------- Order class ---------- //
+
+// Default constructor
+Order::Order() {
+	orderID = 0;
+}
+
+// Parametrized constructor
+Order::Order(int orderID) {
+	this->orderID = orderID;
+}
+
+// Default constructor
+Order::Order(const Order& c) {
+
+}
+
+// Destructor
+Order::~Order() {
+
+}
+
+// Overloads the stream insertion operator.
+ostream& operator<<(ostream& out, const Order& orderOutStream) {
+	out << "The order is being executed" << endl;
+	return out;
+}
+
+// ---------- End of Order class ---------- //
+
+
+// ---------- DeployOrder class ---------- //
+
+// Default constructor
+DeployOrder::DeployOrder() {
+
+}
+
+// Destructor
+DeployOrder::~DeployOrder() {
+
+}
+
+// Stream insertion operator
+ostream& operator<<(ostream& out, const DeployOrder& deployOrderOutStream) {
+	out << "The Deploy order is being executed" << endl;
+	return out;
+}
+
+// Validate Deploy order
+bool DeployOrder::validate() {
+	return true;
+}
+
+// Execute Deploy order if valide
+bool DeployOrder::execute() {
+
+	if (validate()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+
+}
+
+// ---------- End of DeployOrder class ---------- //
+
+
+// ---------- AdvanceOrder class ---------- //
+
+// Default constructor
+AdvanceOrder::AdvanceOrder() {
+
+}
+
+// Destructor
+AdvanceOrder::~AdvanceOrder() {
+
+}
+
+// Stream insertion operator
+ostream& operator<<(ostream& out, const AdvanceOrder& advanceOrderOutStream) {
+	out << "The Advance order is being executed" << endl;
+	return out;
+}
+
+// Validate Advance order
+bool AdvanceOrder::validate() {
+	return true;
+}
+
+// Execute Advance order if valide
+bool AdvanceOrder::execute() {
+
+	if (validate()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+
+}
+
+// ---------- End of AdvanceOrder class ---------- //
+
+
+// ---------- BombOrder class ---------- //
+
+// Default constructor
+BombOrder::BombOrder() {
+
+}
+
+// Destructor
+BombOrder::~BombOrder() {
+
+}
+
+// Stream insertion operator
+ostream& operator<<(ostream& out, const BombOrder& bombOrderOutStream) {
+	out << "The Bomb order is being executed" << endl;
+	return out;
+}
+
+// Validate Bomb order
+bool BombOrder::validate() {
+	return true;
+}
+
+// Execute Bomb order if valide
+bool BombOrder::execute() {
+
+	if (validate()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+
+}
+
+// ---------- End of BombOrder class ---------- //
+
+
+// ---------- BlockadeOrder class ---------- //
+
+// Default constructor
+BlockadeOrder::BlockadeOrder() {
+
+}
+
+// Destructor
+BlockadeOrder::~BlockadeOrder() {
+
+}
+
+// Stream insertion operator
+ostream& operator<<(ostream& out, const BlockadeOrder& blockadeOrderOutStream) {
+	out << "The Blockade order is being executed" << endl;
+	return out;
+}
+
+// Validate Blockade order
+bool BlockadeOrder::validate() {
+	return true;
+}
+
+// Execute Blockade order if valide
+bool BlockadeOrder::execute() {
+
+	if (validate()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+
+}
+
+// ---------- End of BlockadeOrder class ---------- //
+
+
+// ---------- AirliftOrder class ---------- //
+
+// Default constructor
+AirliftOrder::AirliftOrder() {
+
+}
+
+// Destructor
+AirliftOrder::~AirliftOrder() {
+
+}
+
+// Stream insertion operator
+ostream& operator<<(ostream& out, const AirliftOrder& airliftOrderOutStream) {
+	out << "The Airlift order is being executed" << endl;
+	return out;
+}
+
+// Validate Airlift order
+bool AirliftOrder::validate() {
+	return true;
+}
+
+// Execute Airlift order if valide
+bool AirliftOrder::execute() {
+
+	if (validate()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+
+}
+
+// ---------- End of AirliftOrder class ---------- //
+
+
+// ---------- NegotiateOrder class ---------- //
+
+// Default constructor
+NegotiateOrder::NegotiateOrder() {
+
+}
+
+// Destructor
+NegotiateOrder::~NegotiateOrder() {
+
+}
+
+// Stream insertion operator
+ostream& operator<<(ostream& out, const NegotiateOrder& negotiateOrderOutStream) {
+	out << "The Negotiate order is being executed" << endl;
+	return out;
+}
+
+// Validate Negotiate order
+bool NegotiateOrder::validate() {
+	return true;
+}
+
+// Execute Negotiate order if valide
+bool NegotiateOrder::execute() {
+
+	if (validate()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+
+}
+
+// ---------- End of NegotiateOrder class ---------- //
+
+// ---------- OrdersList class ---------- //
+
+// Default constructor
+OrdersList::OrdersList() {
+
+}
+
+// Destructor
+OrdersList::~OrdersList() {
+
+}
+
+void OrdersList::addOrder(Order* order)
+{
+	OrdersVector.push_back(order);
+}
+
+vector<Order*> OrdersList::showOrdersList() {
+	for (int i = 0; i < OrdersVector.size(); i++) {
+		cout << OrdersVector[i] << endl;
+	}
+	return OrdersVector;
+
+}
+
+// Move order from the start index to the end index.
+void OrdersList::move(int startIndex, int endIndex) {
+	iter_swap(OrdersVector.begin() + startIndex, OrdersVector.begin() + endIndex);
+
+}
+
+// Remove order at the specified index.
+void OrdersList::remove(int index) {
+	OrdersVector.erase(OrdersVector.begin() + index);
+
+}
+
+// ---------- End of OrdersList class ---------- //
--- Warzone/Orders.h
+++ Warzone/Orders.h
@@ -1,5 +1,146 @@
 #pragma once
 
+#include <iostream>
+#include <list>
+#include "Map.h"
+#include "Player.h"
+
+using namespace std;
+
 class Order {
-	
+public:
+	// Default Constructor
+	Order();
+
+	// Parameterized Constructor
+	Order(int orderID);
+
+	// Copy Constructor
+	Order(const Order& c);
+
+	// Destructor
+	~Order();
+
+	int orderID = 0;
+
+	// Sends order description to the output stream
+	friend ostream& operator<<(ostream& out, const Order& o);
+
+	virtual bool execute() = 0;
+	virtual bool validate() = 0;
+
+};
+
+// --------------- Order subclasses --------------- //
+
+class DeployOrder : public Order {
+public:	
+	// Constructors
+	DeployOrder();
+
+	// Destructor
+	~DeployOrder();
+
+	// If Deploy order is valid or not. If the order is valid, it will output its effect.
+	bool validate() override;
+
+	// If Deploy order is successfully executed or not.
+	bool execute() override;
+};
+
+class AdvanceOrder : public Order {
+public:
+	// Constructors
+	AdvanceOrder();
+
+	// Destructor
+	~AdvanceOrder();
+
+	// If Advance order is valid or not. If the order is valid, it will output its effect.
+	bool validate() override;
+
+	// If Advance order is successfully executed or not.
+	bool execute() override;
+};
+
+class BombOrder : public Order {
+public:
+	// Constructors
+	BombOrder();
+
+	// Destructor
+	~BombOrder();
+
+	// If Bomb order is valid or not. If the order is valid, it will output its effect.
+	bool validate() override;
+
+	// If Bomb order is successfully executed or not.
+	bool execute() override;
+};
+
+class BlockadeOrder : public Order {
+public:
+	// Constructors
+	BlockadeOrder();
+
+	// Destructor
+	~BlockadeOrder();
+
+	// If Blockade order is valid or not. If the order is valid, it will output its effect.
+	bool validate() override;
+
+	// If Blockade order is successfully executed or not.
+	bool execute() override;
+};
+
+class AirliftOrder : public Order {
+public:
+	// Constructors
+	AirliftOrder();
+
+	// Destructor
+	~AirliftOrder();
+
+	// If Airlift order is valid or not. If the order is valid, it will output its effect.
+	bool validate() override;
+
+	// If Airlift order is successfully executed or not.
+	bool execute() override;
+};
+
+class NegotiateOrder : public Order {
+public:
+	// Constructors
+	NegotiateOrder();
+
+	// Destructor
+	~NegotiateOrder();
+
+	// If Negotiate order is valid or not. If the order is valid, it will output its effect.
+	bool validate() override;
+
+	// If Negotiate order is successfully executed or not.
+	bool execute() override;
+};
+
+
+class OrdersList {
+public:
+	// Constructors
+	OrdersList();
+
+	// Destructor
+	~OrdersList();
+
+	vector<Order*> OrdersVector;
+
+	void addOrder(Order* order);
+
+	vector<Order*> showOrdersList();
+
+	// Move an order in the list of orders
+	void move(int startIndex, int endIndex);
+
+	// Removes the order at the specified position (index)
+	void remove(int index);
 };
