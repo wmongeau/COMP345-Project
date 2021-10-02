c639c78c76f9b1f374c82a74f98e92951e363c11
William Mongeau <williammongeau@gmail.com>
2021-10-02 16:23:28 -0400

    Loop created but not functional due to base class being called

--- Warzone/GameEngineDriver.cpp
+++ Warzone/GameEngineDriver.cpp
@@ -13,9 +13,24 @@ using namespace std;
 int main() {
 	GameEngine *engine;
 	engine = new GameEngine();
-	string currentState = Enums::statesEnumToString(engine -> getCurrentState().getStateName());
-	cout << "You are currently in the " << currentState << "state" << endl;
+	string currentState;
+	do {
+		currentState = Enums::statesEnumToString(engine -> getCurrentState() -> getStateName());
+		cout << "You are currently in the " << currentState << " state" << endl;
+		vector<Transition*> availableTransitions = engine -> getAvailableTransitions();	
+		int index = 0;
 
+		for(Transition* transition : availableTransitions) {
+			cout << index << " " << Enums::transitionsEnumToString(transition -> getTransitionName()) << endl;
+			index++;
+		}
+		
+		cout << "Select a command by entering its number: " << endl;
+		int selected;
+		cin >> selected;
+
+		engine -> execute(availableTransitions[selected]);
+	} while(currentState != "Quit");
 }
 
 std::string Enums::statesEnumToString(Enums::states value){
@@ -44,6 +59,49 @@ std::string Enums::statesEnumToString(Enums::states value){
 		case Enums::winState: {
 			return "Win" ;
 	     	}
+		case Enums::quit: {
+			return "Quit";
+		}
+	}
+
+	return "Error";
+}
+
+std::string Enums::transitionsEnumToString(Enums::transitions value) {
+	switch(value) {
+		case Enums::loadMap: {
+			return "Load Map";
+		}
+		case Enums::validateMap: {
+			return "Validate Map";
+		}
+		case Enums::addPlayer: {
+			return "Add Player";
+		}
+		case Enums::assignCountries: {
+			return "Assign Countries";
+		}
+		case Enums::issueOrder: {
+			return "Issue an Order";
+		}
+		case Enums::endIssueOrders: {
+			return "End Issuing Orders";
+		}
+		case Enums::execOrder: {
+			return "Execute Order";
+		}
+		case Enums::endExecuteOrders: {
+			return "End Executing Orders";
+		}
+		case Enums::winTransition: {
+			return "Win Transition";
+		}
+		case Enums::play: {
+			return "Play Again";
+		}
+		case Enums::end: {
+			return "Quit";
+		}
 	}
 
 	return "Error";
