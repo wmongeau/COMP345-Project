13380ff9996b842677d692daba839e5fe0f0272b
samilmehedi <samil.mehedi@gmail.com>
2021-10-03 15:29:20 -0400

    worked on cards and decks

--- Warzone/Cards.cpp
+++ Warzone/Cards.cpp
@@ -4,6 +4,116 @@
 
 #include <iostream>
 #include <string>
+#include "Cards.h"
 
 using namespace std;
-#include "Cards.h"
+
+const int CARDS = 5;
+
+//Card Constructor
+Card::Card(){
+
+}
+
+//Card Destructor
+Card::~Card(){
+
+}
+
+//Constructor with a string parameter
+Card::Card(string c){
+    this->type = c;
+}
+
+//Copy constructor
+Card::Card(const Card& c) {
+	this->type = c.type;
+}
+
+//method to play a card
+void Card::play(){
+    cout << type << " card is being played" << endl;
+}
+
+string Card::getCardType()
+{
+	return type;
+}
+//Constructor to create a deck of cards
+Deck::Deck(){
+    string type[] = {"Bomb", "Reinforcement", "Blockade", "Airlift", "Diplomacy"};
+
+    for (int count=0; count < CARDS; count++){
+        deckOfCards.push_back(new Card(type[count %5 ]));
+    }
+    cout << "A deck of " << CARDS << " cards has been created" << endl;
+}
+
+//Destructor for a deck of cards
+Deck::~Deck(){
+
+}
+
+// Draws card from deck
+Card* Deck::draw(){
+
+    // shuffling the deck
+    srand((unsigned) time(0));
+    for (int i = 0; i < deckOfCards.size(); i++)
+	{
+        // finds a random value in the deck of card range
+        int j = rand() % deckOfCards.size();
+
+        //shuffles the two cards
+		Card* temp = deckOfCards[i];
+		deckOfCards[i] = deckOfCards[j];
+		deckOfCards[j] = temp;
+	}
+    // remove the last card in the deck and returns it
+    Card* lastCard = deckOfCards.back();
+    Card* drawCard = new Card(*lastCard);
+    deckOfCards.pop_back();
+    delete lastCard;
+    return drawCard;
+}
+
+void Deck::showDeck(){
+    cout << "CURRENT DECK" << endl;
+    for (int i = 0; i < deckOfCards.size(); i++) {
+        cout << i << " "<<this->deckOfCards.at(i)->getCardType() << endl;
+	}
+}
+
+// returns the card back into the deck
+void Deck::returnCardToDeck(Card* c){
+    deckOfCards.push_back(c);
+    // cout << "Card is back in the deck" <<endl;
+    cout << c->getCardType() << " card is back in the deck" << endl;
+}
+
+Hand:: Hand(){
+
+}
+
+Hand::~Hand(){
+
+}
+
+void Hand::selectCard(Card* c)
+{
+	playersHand.push_back(c);
+}
+
+Card* Hand::removeCardFromHand(int i){
+    Card* c = playersHand.at(i);
+    playersHand.erase(playersHand.begin()+i);
+    cout << c->getCardType() << " has been removed from hand" << endl;
+    return c;
+}
+
+void Hand:: showHand(){
+    cout << "CURRENT HAND" << endl;
+	for (int i = 0; i < playersHand.size(); i++) {
+        cout << i << " "<< this->playersHand.at(i)->getCardType() << endl;
+	}
+}
--- Warzone/Cards.h
+++ Warzone/Cards.h
@@ -1,5 +1,48 @@
 #pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
 
 class Card {
+    public:
+        //constructor
+        Card();
+        //destructor
+        ~Card();
+        //copy constructor
+        Card(const Card& c);
+        //assignment operator
+        Card& operator =(const Card& c);
+        //stream insertion operator
+        Card(string c);
+        void play();
+        string getCardType();
+    private:
+        string type;
+};
 
+class Deck {
+    public:
+        Deck();
+        ~Deck();
+        Deck(const Deck& d);
+        Deck& operator = (const Deck& d);
+        vector <Card*> deckOfCards;
+        Card* draw();
+        void returnCardToDeck(Card* c);
+        void showDeck();
+    
+};
+
+class Hand {
+    public:
+        Hand();
+        ~Hand();
+        Hand(const Hand& h);
+        Hand& operator = (const Hand& h);
+        vector <Card*> playersHand;
+        Card* removeCardFromHand(int i);
+        void selectCard(Card* c);
+        void showHand();
 };
