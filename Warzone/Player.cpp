a7395b8ea6db9a81ee4cd4b95864afc26674e718
Patrick <patrickcgodbout@gmail.com>
2021-10-03 14:30:57 -0400

    I thought this was progress byt was wrong

--- Warzone/Player.cpp
+++ Warzone/Player.cpp
@@ -1,3 +1,4 @@
+#include "Player.h"
 /* ------------------------- */
 /* The #include Of this file */
 /* ------------------------- */
@@ -6,6 +7,7 @@
 #include <string>
 
 using namespace std;
+
 #include "Player.h"
 
 Player::Player() {
@@ -30,14 +32,43 @@ vector<Territory*> Player::toAttack() {
 }
 
 vector<Territory*> Player::toDefend() {
-	return OwnedTerritories;
+	return CanDefend;
 }
 
 void Player::addCard(Card* card) {
 	PlayerHand.push_back(card);
 }
 
+void Player::addOwnedTerritory(Territory* territory) {
+	OwnedTerritories.push_back(territory);
+}
+
  void Player::issueOrder() {
 	//create order and issue order here
 	//update CanAttack and OwnedTerritories
 }
+
+Player* playerDriver(Map* map) {
+	Player* player1 = new Player("Will");
+
+	vector<Territory*> owned = vector<Territory*>();
+	vector<Territory*> unowned = vector<Territory*>();
+
+	for (Territory* territory : map->Territories) {
+		if ((rand() % 100 + 1) > 70) {
+			owned.push_back(territory);
+			player1->addOwnedTerritory(territory);
+		}
+	}
+
+	for (Territory* territory : map->Territories) {
+		if ((rand() % 100 + 1) > 70) {
+			unowned.push_back(territory);
+		}
+	}
+
+	player1->setCanDefend(owned);
+	player1->setCanAttack(unowned);
+
+	return player1;
+ }
--- Warzone/Player.h
+++ Warzone/Player.h
@@ -9,13 +9,32 @@ class Player {
 public:
 	Player();
 	Player(std::string playerName) { PlayerName = playerName; };
-	std::vector<Territory*> OwnedTerritories;
-	std::vector<Territory*> CanAttack;
-	std::vector<Card*> PlayerHand;
-	std::string PlayerName;
-	std::vector<Territory*> CanDefend;
+	std::vector<Territory*> getOwnedTerritories() {
+		return OwnedTerritories;
+	}
+	std::vector<Card*> getPlayerHand() {
+		return PlayerHand;
+	}
+	std::string getPlayerName() {
+		return PlayerName;
+	}
+	void setCanDefend(std::vector<Territory*> territories) {
+		CanDefend = territories;
+	}
+	void setCanAttack(std::vector<Territory*> territories) {
+		CanAttack = territories;
+	}
 	std::vector<Territory*> toDefend();
 	std::vector<Territory*> toAttack();
 	void issueOrder();
 	void addCard(Card* card);
+	void addOwnedTerritory(Territory* territory);
+private:
+	std::vector<Territory*> CanDefend;
+	std::vector<Territory*> CanAttack;
+	std::string PlayerName;
+	std::vector<Card*> PlayerHand;
+	std::vector<Territory*> OwnedTerritories;
 };
+
+Player* playerDriver(Map* map);
